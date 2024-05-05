2bc80130bb56979e65116cb9fa8a5b3c987cfdd2
TheDLCrimson <duclnc2004@gmail.com>
2024-05-06 03:01:03 +0700

    yes

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react"
-
 import DeepChatComponent from "~features/deep-chat-component"
 import CurrentTabInfo from "~features/get-tab-info"
 
 function IndexSidePanel() {
-  const [data, setData] = useState("")
-
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 5,
+        padding: 10,
         alignItems: "center"
       }}>
       <DeepChatComponent />
